6b22e1d25eef6ffddec59ae5ca636f1d04e7e569
Eder-R <eder31250@gmail.com>
2023-11-04 22:40:56 -0300

    Logs ignorados

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASS}@{DB_H
 db.init_app(app)
 
 # Configuração do logging
-logging.basicConfig(filename='logs\log.txt', level=logging.ERROR, format='%(asctime)s %(levelname)s %(name)s : %(message)s')
+logging.basicConfig(filename='logs\logs.log.txt', level=logging.ERROR, format='%(asctime)s %(levelname)s %(name)s : %(message)s')
 
 
 @app.route('/', methods=['GET'])

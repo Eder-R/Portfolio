d68e97097726636dc99855fe694bd285e252dcf5
Eder-R <eder31250@gmail.com>
2023-11-30 22:30:41 -0300

    Project Update

--- models/models.py
+++ models/models.py
@@ -1,7 +1,7 @@
 '''Arquivo com os models carregados'''
 from datetime import datetime, timedelta
-
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 app = Flask(__name__)
 db = SQLAlchemy()
